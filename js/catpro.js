f9e6b67a0221219d2e8b67b310f4b0ae0d0a5354
komala <komaladeepthi@gmail.com>
2017-08-13 23:59:57 -0700

    updated admin module

--- js/catpro.js
+++ js/catpro.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     var model = {
     	catCurrent : null,
+    	adminShow : false,
     cats:[{
     		 'catname' : 'redcat',
     		 'catId' : 0,
@@ -53,7 +54,16 @@ $(document).ready(function () {
     	incrementCatCounter: function(){
     		model.catCurrent.count++;
     		 view2.render();
-    	 }	
+    	 },
+
+    	 setAdmin : function(){
+    	 	model.adminShow=true;
+    	 	view3.render();
+    	 },
+
+    	 getAdmin : function(){
+    	 	return model.adminShow;
+    	 }
 
     };
 
@@ -64,30 +74,38 @@ $(document).ready(function () {
     		var $sec=$('#section');
 			var $catList=$('#cat-list');
 			var $catimg=$('#cat-img');
+			var $showform=$('#adminform');
+
 			view1.render();
+    		
 			
     	},
 
     	render : function(){
     		
     		var $sec=$('#section');
+    		var $showform=$('#adminform');
+    		$showform.hide();
     		var catsc=octopus.getCatList();
+
     		
     		$sec.html('');
     		for(var i=0;i<catsc.length;i++)
     		{
     			var catselect=catsc[i];
 
-    			$sec.append('<button id="'+catselect.catId+'">'+catselect.catname+'</button>');
+    			$sec.append('<h3 id="'+catselect.catId+'">'+catselect.catname+'</h3>');
     			var $cat= $('#cat');
     		}
 
-    		 $('button').on('click',function(catcopy){
+    		 $('h3').on('click',function(catcopy){
     				var cat_id=parseInt($(this).attr("id"));
     				var catts=octopus.getCatList();
     				octopus.setCurrentCat(catts[cat_id]);
     				view2.render();
-    				});	
+    				view3.render();
+    		 });
+
     	}
 
     };
@@ -98,11 +116,16 @@ $(document).ready(function () {
     		var $cati= $('#cat-img');
     		var $catname=$('#cat-name');
     		var $catcount=$('#cat-count');
+    		
+    		
+    		
 
     		 $('img').on('click',function(){
     			 octopus.incrementCatCounter();
-    			 // view1.render();    			
+    			 // view1.render(); 
+    		
      });
+    		 
     		view2.render();
     	
     	},
@@ -112,8 +135,7 @@ $(document).ready(function () {
     		var $catname=$('#cat-name');
     		var $catcount=$('#cat-count');
     		
-
-    		var curCat= octopus.getCurrentCat();
+    		curCat= octopus.getCurrentCat();
     		console.log(curCat);
     		var url= curCat.urle;
     		$catname.text(curCat.catname);
@@ -123,7 +145,69 @@ $(document).ready(function () {
     		$cati.attr("alt",curCat.catname);
 
     		
-}
+    		 } 	
+
+
 };
+
+var view3={
+	init: function(){
+		var $showform=$('#adminform');
+		var $submitbutton=$('#submit');
+		var $cancel=$('#cancel');
+
+		view3.render();
+	},
+    
+    render: function(){
+    	var $showform=$('#adminform');
+    	var $cancel=$('#cancel');
+    	var $submit=$('#submit');
+    	catm=octopus.getCurrentCat();
+    	var $newname=$('.newname');
+    	var $newurl=$('.new-url');
+    	var $newclicks=$('.newclicks');
+
+    	var $adminbutton=$('#adminbutton');
+		$('#adminbutton').on('click',function(){
+    		 	octopus.setAdmin();
+    		 });
+
+		var adminkey=octopus.getAdmin();
+    		 if(adminkey==true){
+    		 	$showform.show();
+				}
+
+		$submit.on('click',function(){
+			if($newname!= null){
+				catm.catname=$newname.val();
+			}
+			else{
+				catm.catname=catm.catname;
+			}
+			if($newurl!= null){
+				catm.urle=$newurl.val();
+			}
+			else{
+				catm.urle=catm.urle;
+			}
+			if($newclicks!=null){
+				catm.count=$newclicks.val();
+			}
+			else{
+				catm.count=catm.count;
+			}
+			view1.render();
+			view2.render();
+		})			
+
+
+		$cancel.on('click',function(){
+		$showform.hide();
+		});
+	}
+};
+
     octopus.init();
+
 });
